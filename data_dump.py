f35aaf7f4a714205acf78ca28bf78382e5cb33d2
jainrachit108 <jainrachit124@gmail.com>
2023-02-16 21:08:41 +0530

    final commit

--- data_dump.py
+++ data_dump.py
@@ -6,8 +6,8 @@ from sensor.config import mongo_client
 
 DATABASE_NAME = 'aps'
 COLLECTION_NAME = 'sensor'
-DATA_PATH = '/config/workspace/aps_failure_training_set1.csv'
 
+DATA_PATH = '/config/workspace/aps_failure_training_set1.csv'
 
 if __name__ == '__main__':
     df =pd.read_csv(DATA_PATH)
